b185464441cd8463dfa4e9c845750a12ae2ca815
Maxime Corbin <maxime.corbin@skillz.io>
2021-04-01 17:20:13 +0200

    feat: bootstrap backend with user handling and authentication.

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { User } from '@prisma/client';
+import { compare } from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.user({ email });
+    if (user && await compare(user.password, pass)) {
+      return user;
+    }
+    return null;
+  }
+
+  async register(user: any): Promise<User> {
+    return this.usersService.createUser({
+      username: user.username,
+      password: user.password,
+      email: user.email,
+    });
+  }
+}
