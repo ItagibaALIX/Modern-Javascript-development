f4e5e769ced228d73e17ce3b80aae794d97e6092
oursin <maxime.corbin@epitech.eu>
2021-05-14 15:55:07 +0200

    added user search

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Request, Get, UseGuards } from '@nestjs/common';
+import { Controller, Request, Get, UseGuards, Query } from '@nestjs/common';
 import { AuthenticatedGuard } from '../auth/authenticated.guard';
 import { User } from '@prisma/client';
+import { UsersService } from "./users.service";
+
+class ListUsersDto {
+  email: string;
+}
 
 @UseGuards(AuthenticatedGuard)
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {
+  }
+
   @Get('me')
   async post(@Request() req): Promise<User> {
     console.log('/users/me')
     return req.user
+  }
+
+  @Get('/')
+  async getUsers(@Query() query: ListUsersDto) {
+    return this.usersService.users({
+      take: 10,
+      where: {
+        username: {
+          startsWith: query.email,
+        },
+      },
+    });
   }
 }
--- messages/src/events/events.module.ts
+++ messages/src/events/events.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
 import { AuthModule } from '../auth/auth.module';
+import { EventsService } from './events.service';
+import { PrismaService } from '../db/prisma.service';
 
 @Module({
   imports: [AuthModule],
-  providers: [EventsGateway],
+  providers: [EventsGateway, EventsService, PrismaService],
 })
 export class EventsModule {}
